90159e0a41103e0837121d2512d4f6c1734685fd
Justin Favraud <zehunter6@gmail.com>
2025-12-07 14:51:53 +0100

    Initial Commit

---
+++ Iroel_Project.cpp
@@ -0,0 +1,23 @@
+﻿// Iroel_Project.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Iroel_Project.h"
+#include <SDL3/SDL.h>
+
+using namespace std;
+
+
+static SDL_Window* window = nullptr;
+static SDL_Renderer* renderer = nullptr;
+bool run = true;
+int main()
+{
+	cout << "Hello CMake." << endl;
+	if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_FULLSCREEN, &window, &renderer))
+	{
+		SDL_Log("Couldn't create window and rendrer: %s", SDL_GetError());
+		return -1;
+	}
+
+	return 0;
+}
